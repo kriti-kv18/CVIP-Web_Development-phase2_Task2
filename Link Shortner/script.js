d4050f3dc20325f04226d823847794f158e4dde1
Kriti Verma <114588308+kv-kriti18@users.noreply.github.com>
2023-09-22 19:40:33 +0530

    Add files via upload

---
+++ Link Shortner/script.js
@@ -0,0 +1,16 @@
+async function convertURL()
+{
+    const url= document.getElementById("url").value;
+    const response = await fetch(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`);
+    if(response.ok)
+    {
+        const data = await response.text();
+        document.getElementById('result').innerHTML=`
+        Shortened URL: <a href = "${data}" target="_blank">${data}</a>
+        `;
+    }
+    else
+    {
+        document.getElementById('result').innerHTML = "Error shortening the URL provided";
+    }
+}
